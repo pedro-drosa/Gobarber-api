13c4d64c7a27cb0f789c98445d48d9db2af2cdad
pedro-drosa <pedro_drosa@hotmail.com>
2021-03-26 19:44:31 -0300

    refactoring users module

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import multer from 'multer';
-
 import uploadConfig from '@config/upload';
+
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
@@ -12,21 +13,30 @@ const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
-  const createUserService = new CreateUserService();
+
+  const usersRepository = new UsersRepository();
+  const createUserService = new CreateUserService(usersRepository);
+
   const user = await createUserService.execute({
     name,
     email,
     password,
   });
+
   delete user.password;
+
   return response.json(user);
 });
+
 usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const updateUserAvatarService = new UpdateUserAvatarService();
+    const usersRepository = new UsersRepository();
+    const updateUserAvatarService = new UpdateUserAvatarService(
+      usersRepository,
+    );
 
     const user = await updateUserAvatarService.execute({
       user_id: request.user.id,
