50267a15c698ee0e4783eff7e4f46f02c799a76d
pedro-drosa <pedro_drosa@hotmail.com>
2021-04-03 16:58:49 -0300

    Filtering appointments by month

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,7 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Raw } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 
 import Appointment from '../entities/Appointment';
 
@@ -17,6 +18,25 @@ class AppointmentRepository implements IAppointmentsRepository {
       where: { date },
     });
     return findAppointment || undefined;
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const parsedMonth = String(month).padStart(2, '0');
+
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(
+          dateFieldname =>
+            `to char(${dateFieldname}, 'MM-YYYY' = '${parsedMonth}-${year}')`,
+        ),
+      },
+    });
+    return appointments;
   }
 
   public async create({
